9b4c7a742edbfa473a11db076919eaf2c2ea379f
annamar26 <ag.lo.am26@gmail.com>
2021-11-24 16:41:31 -0600

    Add fake server

--- src/lib/axios/axios.js
+++ src/lib/axios/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 export const getOrders = async() => {
     const resp = await Axios({
-        url: "https://my-json-server.typicode.com/annamar26/CDMX011-burger-queen-api-client/orders",
+        url: "https://fake-server-burguer-queen.herokuapp.com/orders",
     });;
 
     return resp.data
@@ -9,21 +9,21 @@ export const getOrders = async() => {
 };
 export const getProducts = async() => {
     const resp = await Axios({
-        url: "https://my-json-server.typicode.com/annamar26/CDMX011-burger-queen-api-client/products",
+        url: "https://fake-server-burguer-queen.herokuapp.com/products",
     });
     console.log(resp.data)
     return resp.data
 }
 export const orderToKitchen = async(order) => {
 
-    return await Axios.post("https://my-json-server.typicode.com/annamar26/CDMX011-burger-queen-api-client/orders", order);
+    return await Axios.post("https://fake-server-burguer-queen.herokuapp.com/orders", order);
 
 
 };
 export const actualizar = async(objeto, id) => {
 
 
-    return await Axios.put(`https://my-json-server.typicode.com/annamar26/CDMX011-burger-queen-api-client/orders/${id}`, objeto)
+    return await Axios.put(`https://fake-server-burguer-queen.herokuapp.com/${id}`, objeto)
 
 
 
